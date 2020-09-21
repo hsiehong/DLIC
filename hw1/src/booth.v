e717fd1cc13415ef257a0bedb71422fe89924639
hsiehong <hsiehong1022@gmail.com>
2020-09-21 17:18:14 +0800

    first commit

---
+++ hw1/src/booth.v
@@ -0,0 +1,14 @@
+`timescale 1ns / 10ps
+module booth(out, in1, in2);
+
+parameter width = 6;
+
+input  	[width-1:0]   in1; //multiplicand
+input  	[width-1:0]   in2; //multiplier
+output  [2*width-1:0] out; //product
+
+
+/*write your code here*/
+
+
+endmodule
